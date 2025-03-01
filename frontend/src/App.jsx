612f62e3e52d3e48813cb7bef563f0b1b628618b
dumprahul <shanrahul04@gmail.com>
2025-03-01 15:31:45 +0530

    appjsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,18 @@
+import "./styles/global.css";
+import "./styles/TextMessage.css";
+import "./styles/SceneTransition.css";
+import "./styles/KeyboardMenu.css";
+import "./styles/Hud.css";
+import "./styles/TitleScreen.css";
+import "./styles/Battle.css";
+import "./styles/Combatant.css";
+import "./styles/SubmissionMenu.css";
+import "./styles/Team.css";
+import "./styles/Menus.css";
+
+
+
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
